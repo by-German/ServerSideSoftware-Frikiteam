189abc5fd405ae9779bf469f4d10122b4f238edc
by-German <german280599@gmail.com>
2021-05-03 02:29:31 -0500

    init

---
+++ src/main/java/com/frikiteam/events/EventsApplication.java
@@ -0,0 +1,22 @@
+package com.frikiteam.events;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class EventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
