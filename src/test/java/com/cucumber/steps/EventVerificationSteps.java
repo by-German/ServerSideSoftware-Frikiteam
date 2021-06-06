3b6e96996148348407fd2b97c0d6ab04cd3d8b32
by-German <german280599@gmail.com>
2021-06-06 13:00:31 -0500

    test-config-finished

--- src/test/java/com/cucumber/steps/EventVerificationSteps.java
+++ src/test/java/com/cucumber/steps/EventVerificationSteps.java
@@ -1,107 +1,31 @@
 package com.cucumber.steps;
 
-import com.cucumber.RunCucumberTest;
-import com.frikiteam.events.EventsApplication;
-import com.frikiteam.events.controller.OffersController;
-import com.frikiteam.events.controller.TagsController;
-import com.frikiteam.events.domain.model.Offer;
-import com.frikiteam.events.domain.model.Tag;
-import com.frikiteam.events.domain.repositories.OfferRepository;
-import com.frikiteam.events.domain.service.OfferService;
-import com.frikiteam.events.resource.OfferResource;
-import com.frikiteam.events.resource.TagResource;
-import com.frikiteam.events.service.OfferServiceImpl;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootContextLoader;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.Mockito.when;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = RunCucumberTest.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EventVerificationSteps {
-    private RestTemplate restTemplate = new RestTemplate();
-//     Implement Mock de repository de test
-//    @MockBean
-//    private OfferRepository repository;
-//    private OffersController controller = new OffersController();
-//
-//    @TestConfiguration
-//    static class EventServiceTestConfiguration {
-//        @Bean
-//        public OfferService employeeService() {
-//            return new OfferServiceImpl();
-//        }
-//        @Bean
-//        public ModelMapper modelMapper() {
-//            return new ModelMapper();
-//        }
-//    }
 
 
     @Given("the user is in the event list")
     public void userIsInTheEventList() {
 
-
-
     }
 
     @When("viewing the status of each event")
     public void viewingTheStatusOfEachEvent() {
-        String url = "http://localhost:8080/api/offers/1";
-//        String url = "https://api-frikiteam.herokuapp.com/api/offers/1";
-        OfferResource offer = restTemplate.getForObject(url, OfferResource.class);
-        System.out.println(offer.getName());
 
-
-
-//        Long id = 1L;
-//        Offer offer = new Offer();
-//        offer.setId(id);
-//        offer.setName("name of offer");
-//        OfferResource resource = new OfferResource();
-//        resource.setName("name of offer");
-//
-//        when(repository.findById(id)).thenReturn(Optional.of(offer));
-//
-//        OfferResource result =  controller.getByIdOffer(id);
-//        System.out.println(result.getName());
     }
 
     @Then("the system indicates Verified")
     public void systemIndicatesVerified() {
-        int a = 1;
-        int b = 1;
-        assert(a == b);
+
     }
 
 
     @Given("Since the user is in the event list")
     public void asd() {
-        String url = "http://localhost:8080/api/offers/1";
-//        String url = "https://api-frikiteam.herokuapp.com/api/offers/1";
-        OfferResource offer = restTemplate.getForObject(url, OfferResource.class);
-        System.out.println(offer.getName());
+
     }
 
     @When("you view the status of each event you choose")
@@ -111,9 +35,7 @@ public class EventVerificationSteps {
 
     @Then("the system indicates Not Verified")
     public void zxc() {
-        int a = 2;
-        int b = 2;
-        assert(a == b);
+
     }
 
 }
--- src/test/java/com/cucumber/steps/SearchByEventNameSteps.java
+++ src/test/java/com/cucumber/steps/SearchByEventNameSteps.java
@@ -1,29 +1,57 @@
 package com.cucumber.steps;
 
-import com.frikiteam.events.resource.OfferResource;
+import com.frikiteam.events.domain.model.Event;
+import com.frikiteam.events.resource.EventResource;
+import com.frikiteam.events.resource.SaveEventResource;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.web.client.RestTemplate;
 
 public class SearchByEventNameSteps {
-    private RestTemplate restTemplate = new RestTemplate();
+    private final String url = "http://localhost:8080/api/";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private Event result;
+    private String name;
 
     @Given("exist a event with name {string} im a list of events")
     public void asd(String name) {
+        // Arrange
+        // verify if event exists -> else: to create it
+//        Event exist = restTemplate.getForObject(url + "events/?name=" + name, Event.class);
+//        if (exist == null) {
+            SaveEventResource resource = new SaveEventResource();
+            resource.setName(name);
+            resource.setInformation("informacion del evento numero 5");
+            resource.setPrice(10.0);
+            resource.setQuantity(100);
+            restTemplate.postForObject(url + "organizers/1/events/places/1", resource, EventResource.class);
+//        }
+
+
+//        String getOfferId = "offers/1";
+//        Offer offer = restTemplate.getForObject(url + getOfferId, Offer.class);
+//        if (offer != null) {
+//            System.out.println(offer.getId());
+//            System.out.println(offer.getName());
+//            System.out.println(offer.getDiscountAmount());
+//        }
 
     }
 
     @When("the user search the event with name {string}")
     public void sdf(String name) {
-        String url = "http://localhost:8080/api/offers/1";
-//        String url = "https://api-frikiteam.herokuapp.com/api/offers/1";
-        OfferResource offer = restTemplate.getForObject(url, OfferResource.class);
-        System.out.println(offer.getName());
+        // Action -> endpoint search event by name
+//        this.name = name;
+//        result = restTemplate.getForObject("events/?name=jose", Event.class);
+//        EventInformation ef = restTemplate.getForObject("events/1/information", EventInformation.class);
     }
 
     @Then("the event with the searched name is displayed")
     public void dfg() {
-
+//        Assert that the event is equals to result
+//        assert (result != null);
+//        assert (result.getName() == name);
     }
 }
