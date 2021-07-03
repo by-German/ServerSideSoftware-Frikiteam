9f9f77130f997e0f3f59f5a4e9567f938e7f77aa
by-German <german280599@gmail.com>
2021-07-03 10:28:10 -0500

    fix: size img

--- src/main/java/com/frikiteam/events/domain/model/EventInformation.java
+++ src/main/java/com/frikiteam/events/domain/model/EventInformation.java
@@ -16,6 +16,7 @@ public class EventInformation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @Lob
     private String description;
     private String image;
 
