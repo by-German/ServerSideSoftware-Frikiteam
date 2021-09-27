67e77cb69d49e3b189b2b0820bef572fd4d65ce7
by-German <german280599@gmail.com>
2021-09-27 03:12:01 -0500

    feat: searh user by email

--- src/main/java/com/frikiteam/events/domain/repositories/UserRepository.java
+++ src/main/java/com/frikiteam/events/domain/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.frikiteam.events.domain.repositories;
 
 import com.frikiteam.events.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Transactional
     User findByEmail(String email);
 }
